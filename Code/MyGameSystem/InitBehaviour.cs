596ab789baead52a0f35b7f56f68efc2a47fc29d
Minstreams <minstreams@qq.com>
2018-07-17 12:03:15 +0800

    ~

---
+++ Code/MyGameSystem/InitBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using GameSystem.InnerSystem;
+
+/// <summary>
+/// 带初始化方法的Behaviour，（禁用awake）
+/// </summary>
+public abstract class InitBehaviour : MonoBehaviour {
+    protected virtual void Awake()
+    {
+        GameMessageManager.gameInit += Init;
+    }
+
+    private void OnDestroy()
+    {
+        GameMessageManager.gameInit -= Init;
+    }
+    protected abstract void Init();
+}
