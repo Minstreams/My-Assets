a1b6f225992a69c88a6e94c6f0ff0cb69e22915e
Minstreams <32974920+Minstreams@users.noreply.github.com>
2021-01-14 04:49:21 +0000

    InputSystem Update

---
+++ TheMatrix/Assets/SubSystem/InputSystem/Operator/InputKeyTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSystem.Operator
+{
+
+    [AddComponentMenu("[InputSystem]/Operator/InputKeyTrigger")]
+    public class InputKeyTrigger : MonoBehaviour
+    {
+        [MinsHeader("Operator of InputSystem", SummaryType.PreTitleOperator, -1)]
+        [MinsHeader("InputKeyTrigger", SummaryType.TitleOrange, 0)]
+        [MinsHeader("", SummaryType.CommentCenter, 1)]
+        [Label] public InputKey key;
+        public SimpleEvent onKey;
+        public SimpleEvent onKeyDown;
+        public SimpleEvent onKeyUp;
+
+        void Update()
+        {
+            if (InputSystem.GetKey(key)) onKey?.Invoke();
+            if (InputSystem.GetKeyDown(key)) onKeyDown?.Invoke();
+            if (InputSystem.GetKeyUp(key)) onKeyUp?.Invoke();
+        }
+
+    }
+}
