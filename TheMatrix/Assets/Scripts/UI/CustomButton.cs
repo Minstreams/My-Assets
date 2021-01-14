311572edb2e475007934ecbcb41a5640264997a4
Minstreams <32974920+Minstreams@users.noreply.github.com>
2021-01-14 04:41:01 +0000

    Create CustomButton.cs

---
+++ TheMatrix/Assets/Scripts/UI/CustomButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSystem.UI
+{
+
+    [AddComponentMenu("|UI/CustomButton")]
+    public class CustomButton : UIBase
+    {
+        [MinsHeader("Make a button on GUI to trigger an event.", SummaryType.CommentCenter, -1)]
+        [Label] public string buttonText;
+
+        public SimpleEvent output;
+        protected override void OnUI()
+        {
+            if (GUILayout.Button(buttonText)) output?.Invoke();
+        }
+        protected override void Reset()
+        {
+            base.Reset();
+            SetStyle(ref boxStyle, "label");
+        }
+    }
+}
